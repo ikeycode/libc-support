0079fa06f67dcefd28044d8e56163380069354c2
Jouni Roivas <jroivas@iki.fi>
2020-08-20 23:28:49 +0300

    Match variable, print result
    Signed-off-by: Jouni Roivas <jroivas@iki.fi>

--- getconf.c
+++ getconf.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+typedef struct {
+    const char *name;
+    int key;
+} sysconf_var_t;
+
+const sysconf_var_t sysconf_vars[] = {
+    { "ARG_MAX", _SC_ARG_MAX },
+};
 
 void err(const char *msg, ...)
 {
@@ -15,6 +26,13 @@ void err(const char *msg, ...)
 
 int print_variable(const char *var)
 {
+    const sysconf_var_t *item = sysconf_vars;
+
+    if (strcmp(item->name, var) == 0) {
+        printf("%ld\n", sysconf(item->key));
+        return 0;
+    }
+
     err("Unrecognized variable: '%s'\n", var);
     return 1;
 }
