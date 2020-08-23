901f6c06d95ae246447d676bc4932ef1b2623b64
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-08-23 21:00:41 +0100

    getconf: Add missing modelines
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- src/getconf/getconf.c
+++ src/getconf/getconf.c
@@ -326,3 +326,16 @@ int main(int argc, char **argv)
 
         return 0;
 }
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
