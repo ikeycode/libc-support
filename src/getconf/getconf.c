476f652ad979d1cc8ac919059758f8c9862fdd38
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-08-23 20:47:43 +0100

    getconf: Modernise the usage and implementation of the tool
    We're now implemented in a more maintainable way by having a single
    merged list of all known variables, and tagging each option by using
    easy-to-use macros within an include file.
    
    The new GET_* macros make it trivial to add support for new variables
    and aliases provided there is compile-time support for finding them.
    This will allow us to very quickly get to a POSIX-compliant tool.
    
    Note we do still ignore the version specification argument, despite
    handling it within our revised getopt_long usage, saving manual
    CLI parsing.
    
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- src/getconf/getconf.c
+++ src/getconf/getconf.c
@@ -1,288 +1,328 @@
-#include <stdarg.h>
+#define _GNU_SOURCE
+
+#include <getopt.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-typedef struct {
-        const char *name;
-        int key;
-} conf_var_t;
+#include "config.h"
 
-static const conf_var_t sysconf_vars[] = {
-        /* POSIX.1 variables from man sysconf */
-        { "ARG_MAX", _SC_ARG_MAX },
-        { "CHILD_MAX", _SC_CHILD_MAX },
-        { "HOST_NAME_MAX", _SC_HOST_NAME_MAX },
-        { "LOGIN_NAME_MAX", _SC_LOGIN_NAME_MAX },
-        { "NGROUPS_MAX", _SC_NGROUPS_MAX },
-        { "OPEN_MAX", _SC_OPEN_MAX },
-        { "PAGESIZE", _SC_PAGESIZE },
-        { "RE_DUP_MAX", _SC_RE_DUP_MAX },
-        { "STREAM_MAX", _SC_STREAM_MAX },
-        { "SYMLOOP_MAX", _SC_SYMLOOP_MAX },
-        { "TTY_NAME_MAX", _SC_TTY_NAME_MAX },
-        { "TZNAME_MAX", _SC_TZNAME_MAX },
-        { "_POSIX_VERSION", _SC_VERSION },
+/**
+ * How we'll find a given variable by name.
+ */
+typedef enum {
+        LOOKUP_SYSCONF = 0,  /**< Lookup in sysconf() */
+        LOOKUP_PATHCONF = 1, /**< Lookup in pathconf() */
+        LOOKUP_CONFSTR = 2,  /**< Lookup in confstr() */
+        LOOKUP_DEFINE = 3,   /**< Already defined, print it. */
+} LookupMethod;
 
-        /* POSIX.2 variables from man sysconf */
-        { "BC_BASE_MAX", _SC_BC_BASE_MAX },
-        { "BC_DIM_MAX", _SC_BC_DIM_MAX },
-        { "BC_SCALE_MAX", _SC_BC_SCALE_MAX },
-        { "COLL_WEIGHTS_MAX", _SC_COLL_WEIGHTS_MAX },
-        { "EXPR_NEST_MAX", _SC_EXPR_NEST_MAX },
-        { "LINE_MAX", _SC_LINE_MAX },
-        { "RE_DUP_MAX", _SC_RE_DUP_MAX },
-        { "POSIX2_VERSION", _SC_2_VERSION },
-        { "POSIX2_C_DEV", _SC_2_C_DEV },
-        { "POSIX2_FORT_DEV", _SC_2_FORT_DEV },
-        { "POSIX2_FORT_RUN", _SC_2_FORT_RUN },
-        { "_POSIX2_LOCALEDEF", _SC_2_LOCALEDEF },
-        { "POSIX2_SW_DEV", _SC_2_SW_DEV },
+/**
+ * SystemConfigVariable encapsulates various getconf variables, notably:
+ *
+ * - sysconf
+ * - confstr
+ * - pathconf
+ *
+ * We used a singular merged list of all variables to make it far easier
+ * to maintain this utility.
+ */
+typedef struct SystemConfigVariable {
+        const char *name; /**<Name of the variale */
+        union {
+                long long lkey; /**< Long key */
+                short skey;     /**< Short key */
+        };
+        bool unsign;         /**< Signed/unsigned */
+        LookupMethod method; /**< Where to retrieve variable */
+} SystemConfigVariable;
 
-        /* Others spotted from "getconf -a" list */
-        { "ATEXIT_MAX", _SC_ATEXIT_MAX },
-        { "CHAR_BIT", _SC_CHAR_BIT },
-        { "CHAR_MIN", _SC_CHAR_MIN },
-        { "CHAR_MAX", _SC_CHAR_MAX },
-        { "SHRT_MIN", _SC_SHRT_MIN },
-        { "SHRT_MAX", _SC_SHRT_MAX },
-        { "INT_MIN", _SC_INT_MIN },
-        { "INT_MAX", _SC_INT_MAX },
-        { "UCHAR_MAX", _SC_UCHAR_MAX },
-        { "USHRT_MAX", _SC_USHRT_MAX },
-        { "UINT_MAX", _SC_UINT_MAX },
-        { "ULONG_MAX", _SC_ULONG_MAX },
-        { "WORD_BIT", _SC_WORD_BIT },
-        { "IOV_MAX", _SC_IOV_MAX },
-};
-
-static const conf_var_t confstr_vars[] = {
-        { "PATH", _CS_PATH },
-};
-
-/* Variables from man pathconf */
-static const conf_var_t pathconf_vars[] = {
-        { "LINK_MAX", _PC_LINK_MAX },
-        { "MAX_CANON", _PC_MAX_CANON },
-        { "MAX_INPUT", _PC_MAX_INPUT },
-        { "NAME_MAX", _PC_NAME_MAX },
-        { "PATH_MAX", _PC_PATH_MAX },
-        { "PIPE_BUF", _PC_PIPE_BUF },
-        { "CHOWN_RESTRICTED", _PC_CHOWN_RESTRICTED },
-        { "NO_TRUNC", _PC_NO_TRUNC },
-        { "VDISABLE", _PC_VDISABLE },
-};
-
-static const size_t sysconf_var_cnt = sizeof(sysconf_vars) / sizeof(conf_var_t);
-static const size_t confstr_var_cnt = sizeof(confstr_vars) / sizeof(conf_var_t);
-static const size_t pathconf_var_cnt = sizeof(pathconf_vars) / sizeof(conf_var_t);
-
-static const long text_align_to_chars = 25;
-static const int NOT_IN_LIST = -1;
-
-enum { HELP_SHORT, HELP_FULL };
-
-static void err(const char *msg, ...)
-{
-        va_list args;
-
-        /*@-nullpass@*/
-        va_start(args, msg);
-        (void)vfprintf(stderr, msg, args);
-        va_end(args);
-        /*@=nullpass@*/
-
-        exit(EXIT_FAILURE);
-}
-
-static int print_sysconf(int val)
-{
-        long res = sysconf(val);
-
-        if (val == _SC_ULONG_MAX)
-                printf("%lu\n", (unsigned long)res);
-        else if (res == -1)
-                printf("undefined\n");
-        else
-                printf("%ld\n", res);
-
-        return 0;
-}
-
-static int print_confstr(int val)
-{
-        /*@-nullpass@*/
-        size_t len = confstr(val, NULL, 0);
-        /*@=nullpass@*/
-        char *res = NULL;
-
-        if (len == 0) {
-                printf("undefined\n");
-                return 0;
+/**
+ * The variable is callable via sysconf() interface
+ */
+#define GET_SYSCONF_VARIABLE(N, K)                                                                 \
+        {                                                                                          \
+                .name = N, .skey = K, .method = LOOKUP_SYSCONF, .unsign = false                    \
         }
 
-        res = calloc(1, len + 1);
-        if (res == NULL)
-                err("Out of memory");
-        else if (confstr(val, res, len) == len)
-                printf("%s\n", res);
-        else
-                printf("undefined\n");
-        free(res);
-
-        return 0;
-}
-
-static int print_pathconf(int val, /*@null@*/ const char *path)
-{
-        long res = 0;
-
-        if (path == NULL)
-                return 0;
-
-        res = pathconf(path, val);
-        if (res == -1)
-                printf("undefined\n");
-        else
-                printf("%ld\n", res);
-
-        return 0;
-}
-
-static int in_list(const conf_var_t *vars, size_t cnt, /*@null@*/ const char *var)
-{
-        const conf_var_t *max_item = vars + cnt;
-        const conf_var_t *item = NULL;
-
-        if (var == NULL || vars == NULL)
-                return NOT_IN_LIST;
-
-        for (item = vars; item < max_item; item++) {
-                if (item->name != NULL && strcmp(item->name, var) == 0)
-                        return item->key;
+/**
+ * The variable is compiler-defined, and is a signed number
+ */
+#define GET_SIGNED_DEFINITION(N, K)                                                                \
+        {                                                                                          \
+                .name = N, .lkey = K, .method = LOOKUP_DEFINE, .unsign = false                     \
         }
 
-        return NOT_IN_LIST;
-}
-
-static int print_variable(/*@null@*/ const char *var, /*@null@*/ const char *path)
-{
-        int res = 0;
-
-        if (var == NULL)
-                err("No variable\n");
-
-        res = in_list(sysconf_vars, sysconf_var_cnt, var);
-        if (res != NOT_IN_LIST)
-                return print_sysconf(res);
-
-        res = in_list(confstr_vars, confstr_var_cnt, var);
-        if (res != NOT_IN_LIST)
-                return print_confstr(res);
-
-        if (path != NULL) {
-                res = in_list(pathconf_vars, pathconf_var_cnt, var);
-                if (res != NOT_IN_LIST)
-                        return print_pathconf(res, path);
+/**
+ * The variable is compiler-defined, and is an unsigned number
+ */
+#define GET_UNSIGNED_DEFINITION(N, K)                                                              \
+        {                                                                                          \
+                .name = N, .lkey = (long long)K, .method = LOOKUP_DEFINE, .unsign = true           \
         }
 
-        /*@-nullpass@*/
-        err("Unrecognized variable: '%s'\n", var);
-        /*@=nullpass@*/
+/**
+ * The variable is callable via confstr() interface
+ */
+#define GET_CONFSTR_VARIABLE(N, K)                                                                 \
+        {                                                                                          \
+                .name = N, .lkey = K, .method = LOOKUP_CONFSTR, .unsign = false                    \
+        }
 
-        return 1;
+/**
+ * The variable is callable via pathconf() interface
+ */
+#define GET_PATHCONF_VARIABLE(N, K)                                                                \
+        {                                                                                          \
+                .name = N, .lkey = K, .method = LOOKUP_PATHCONF, .unsign = false                   \
+        }
+
+/**
+ * All of our variables come from getconf.inc to make it easier to
+ * maintain
+ */
+static SystemConfigVariable system_config_vars[] = {
+#include "getconf.inc"
+};
+
+/**
+ * Return the length of an array with known storage size at compile time
+ */
+#define ARRAY_SIZE(v) sizeof(v) / sizeof(v[0])
+
+/**
+ * Print a single variable and its corresponding value
+ */
+static inline void print_one(const SystemConfigVariable *v, const char *filename)
+{
+        switch (v->method) {
+        case LOOKUP_SYSCONF:
+                fprintf(stdout, "%ld\n", sysconf(v->skey));
+                break;
+        case LOOKUP_DEFINE:
+                if (v->unsign) {
+                        fprintf(stdout, "%llu\n", (unsigned long long)v->lkey);
+                } else {
+                        fprintf(stdout, "%lld\n", v->lkey);
+                }
+                break;
+        case LOOKUP_CONFSTR: {
+                char *buffer = NULL;
+                size_t sz = 0;
+
+                sz = confstr((int)v->lkey, buffer, 0);
+                if (sz <= 0) {
+                        return;
+                }
+                /* Ensure we can allocate */
+                buffer = calloc(sz, sizeof(char));
+                if (!buffer) {
+                        abort();
+                }
+                /* Should be the same size this time! */
+                if (confstr((int)v->lkey, buffer, sz) != sz) {
+                        free(buffer);
+                        abort();
+                }
+                fprintf(stdout, "%s\n", buffer);
+                free(buffer);
+                break;
+        }
+        case LOOKUP_PATHCONF:
+                fprintf(stdout, "%ld\n", pathconf(filename == NULL ? "." : filename, v->skey));
+                break;
+        default:
+                fprintf(stdout, "\n");
+        }
 }
 
-static void print_aligned_to(const char *msg, long align_to)
+/**
+ * Print all variables and their corresponding values
+ */
+static void print_all(const char *filename)
 {
-        long cnt = 0;
+        size_t largest = 0;
+        /* Compute largest name. */
+        for (uint16_t i = 0; i < ARRAY_SIZE(system_config_vars); i++) {
+                size_t newsize = strlen(system_config_vars[i].name);
+                if (newsize > largest) {
+                        largest = newsize;
+                }
+        }
 
-        if (msg == NULL)
-                return;
+        for (uint16_t i = 0; i < ARRAY_SIZE(system_config_vars); i++) {
+                SystemConfigVariable *v = &system_config_vars[i];
+                fprintf(stdout, "%-*s\t", (int)largest, v->name);
+                print_one(v, filename);
+        }
+}
 
-        printf("%s ", msg);
-        cnt = align_to - (long)strlen(msg);
-        if (cnt <= 0)
-                return;
+/**
+ * Return the SystemConfigVariable for the given variable name.
+ * Returns NULL ("undefined") if it cannot be found
+ */
+static const SystemConfigVariable *find_variable(const char *name)
+{
+        for (uint16_t i = 0; i < ARRAY_SIZE(system_config_vars); i++) {
+                const SystemConfigVariable *var = &system_config_vars[i];
+                if (var && var->name && name && strcmp(name, var->name) == 0) {
+                        return var;
+                }
+        }
+        return NULL;
+}
 
-        while (cnt-- > 0) {
-                if (fputc(' ', stdout) == EOF)
+/**
+ * Program arguments.
+ */
+static struct option prog_opts[] = {
+        { "version-specification", no_argument, 0, 'v' },
+        {
+            "version",
+            no_argument,
+            0,
+            'V',
+        },
+        { "help", no_argument, 0, 'h' },
+        { "all", no_argument, 0, 'a' },
+        { NULL, 0, 0, 0 },
+};
+
+/**
+ * Print correct CLI usage of the tool
+ */
+static void printUsage(const char *progname)
+{
+        fprintf(stdout, "Usage: %s [-v specification] variable_name [pathname]\n", progname);
+        fprintf(stdout, "       %s -a [pathname]\n", progname);
+}
+
+/**
+ * Pretty-print a help message
+ */
+static void printHelp(const char *progname)
+{
+        printUsage(progname);
+
+        fputs("    -v, --version-specification          Set the version specification\n", stdout);
+        fputs("    -h, --help                           Display this help message\n", stdout);
+        fputs("    -a, --all                            Display all applicable variables\n",
+              stdout);
+        fputs("    -V, --version                        Display program version and quit\n",
+              stdout);
+}
+
+/**
+ * Print our version information
+ */
+static void printVersion(void)
+{
+        fputs("getconf version " PACKAGE_VERSION " \n\n", stdout);
+        fputs("Copyright © 2020 Serpent OS Developers\n", stdout);
+        fputs("Part of the libc-support project\n", stdout);
+        fputs("Available under the terms of the MIT license\n", stdout);
+}
+
+/**
+ * Main entry point into the program
+ */
+int main(int argc, char **argv)
+{
+        int opt = 0;
+        __attribute__((unused)) const char *specification = NULL;
+        const SystemConfigVariable *variable = NULL;
+        bool process_loop = true;
+        /* Stash before winding */
+        const char *progname = argv[0];
+        bool listing = false;
+
+        while (process_loop) {
+                int option_index = 0;
+                opt = getopt_long(argc, argv, "ahVv:", prog_opts, &option_index);
+
+                switch (opt) {
+                case 'h':
+                        printHelp(progname);
+                        return EXIT_SUCCESS;
                         break;
-        }
-}
+                case 'v':
+                        specification = optarg;
+                        break;
+                case 'V':
+                        printVersion();
+                        return EXIT_SUCCESS;
+                case 'a':
+                        listing = true;
+                        break;
+                case -1:
+                        process_loop = false;
+                        break;
+                case '?':
+                        printUsage(progname);
+                        return EXIT_FAILURE;
+                default:
+                        break;
+                }
 
-static int print_all(/*@null@*/ const char *path)
-{
-        const conf_var_t *item = NULL;
+                if (!process_loop) {
+                        break;
+                }
+        }
 
-        for (item = sysconf_vars; item < sysconf_vars + sysconf_var_cnt; item++) {
-                print_aligned_to(item->name, text_align_to_chars);
-                (void)print_sysconf(item->key);
+        argc -= optind;
+        argv += optind;
+
+        /**
+         * When listing, we accept only 1 argument, for pathconf() usage
+         */
+        if (listing) {
+                const char *pathscan = NULL;
+                if (argc == 1) {
+                        pathscan = argv[0];
+                } else if (argc > 1) {
+                        printUsage(progname);
+                        return EXIT_FAILURE;
+                }
+
+                print_all(pathscan);
+                return EXIT_SUCCESS;
         }
-        for (item = confstr_vars; item < confstr_vars + confstr_var_cnt; item++) {
-                print_aligned_to(item->name, text_align_to_chars);
-                (void)print_confstr(item->key);
-        }
-        for (item = pathconf_vars; item < pathconf_vars + pathconf_var_cnt; item++) {
-                print_aligned_to(item->name, text_align_to_chars);
-                (void)print_pathconf(item->key, path != NULL ? path : ".");
+
+        /**
+         * Process according to the number of arguments. A filepath means
+         * we'll only deal with PATHCONF variables.
+         */
+        switch (argc) {
+        /* sysconf/defines/confstr */
+        case 1:
+                variable = find_variable(argv[0]);
+                if (!variable) {
+                        fputs("undefined\n", stdout);
+                        return EXIT_SUCCESS;
+                }
+                print_one(variable, NULL);
+                break;
+        /* pathconf only */
+        case 2:
+                variable = find_variable(argv[0]);
+                if (!variable) {
+                        return EXIT_SUCCESS;
+                }
+
+                if (variable->method != LOOKUP_PATHCONF) {
+                        fputs("undefined\n", stdout);
+                        return EXIT_SUCCESS;
+                }
+                print_one(variable, argv[1]);
+                break;
+        default:
+                printUsage(progname);
+                break;
         }
 
         return 0;
-}
-
-static void usage(const char *name, int help)
-{
-        fprintf(stderr, "Usage: %s [-v specification] variable [path]\n", name);
-        fprintf(stderr, "       %s -a [path]\n", name);
-
-        if (help == HELP_FULL) {
-                fprintf(stderr, "\nArguments:\n");
-                fprintf(stderr,
-                        "  -a           Display all configuration variables and their values\n");
-                fprintf(stderr,
-                        "  -v spec      Indicate the specification and version to obtain "
-                        "configuration\n");
-                fprintf(stderr, "               variable for\n");
-                fprintf(stderr, "  variable     The variable name to get value for\n");
-                fprintf(stderr,
-                        "  path         File system path to get pathconf(3) variable from\n");
-        }
-
-        exit(EXIT_FAILURE);
-}
-
-int main(int argc, const char **argv)
-{
-        const char *varname = NULL;
-        const char *pathname = NULL;
-        int all = 0;
-        int index = 1;
-
-        if (argc <= 1)
-                usage(argv[0], HELP_SHORT);
-
-        for (index = 1; index < argc; index++) {
-                if (strcmp("-a", argv[index]) == 0)
-                        all = 1;
-                else if (strcmp("-h", argv[index]) == 0 || strcmp("--help", argv[index]) == 0)
-                        usage(argv[0], HELP_FULL);
-                else if (strcmp("-v", argv[index]) == 0) {
-                        index++;
-                        if (index >= argc)
-                                usage(argv[0], HELP_SHORT);
-                        /* Value ignored for now */
-                } else if (all == 0 && varname == NULL)
-                        varname = argv[index];
-                else if (pathname == NULL)
-                        pathname = argv[index];
-                else
-                        err("Invalid argument: %s\n", argv[index]);
-        }
-
-        if (all == 1)
-                return print_all(pathname);
-
-        return print_variable(varname, pathname);
 }
